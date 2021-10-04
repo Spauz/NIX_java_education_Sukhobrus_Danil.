a4ddaa91bd74ff4895229c9f4aeafab21c0622ba
Spauz <d.o.sukhobrus@student.khai.edu>
2021-10-04 19:24:19 +0300

    Resolved conflict

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,9 @@
 public class Test {
     public static void main (String[] args){
-        System.out.println("Java");
+        System.out.println("Java education");
+        //new string
+        System.out.println("DICT");
     }
 }
+
+
